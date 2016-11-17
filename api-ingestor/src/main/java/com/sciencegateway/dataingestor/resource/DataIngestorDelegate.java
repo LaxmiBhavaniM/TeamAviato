2f1f86623667a9877d0be3735bb0d745a0e46fb4
Sneha Tilak <sneha.tilak26@gmail.com>
2016-11-16 23:18:01 -0500

    Adding Zookeeper classes #36

---
+++ api-ingestor/src/main/java/com/sciencegateway/dataingestor/resource/DataIngestorDelegate.java
@@ -0,0 +1,101 @@
+package com.sciencegateway.dataingestor.resource;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.ServiceProvider;
+import org.apache.curator.x.discovery.UriSpec;
+import org.apache.log4j.Logger;
+
+@Path("/ingestor")
+public class DataIngestorDelegate 
+{
+
+	private static Logger logger = Logger.getLogger(DataIngestorDelegate.class);
+
+	@GET
+	@Path("/try")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String generateString() throws IOException
+	{
+		return "Got it!";
+	}
+	
+	@SuppressWarnings("unused")
+	@GET
+	@Path("/delegate")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String delegate() 
+	{
+		logger.info("Inside Delegator...");
+		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181", new RetryNTimes(5, 1000));
+		curatorFramework.start();
+		ServiceDiscovery<Void> serviceDiscovery = ServiceDiscoveryBuilder.builder(Void.class).basePath("weather-predictor").client(curatorFramework).build();
+		
+		try 
+		{
+			serviceDiscovery.start();
+		} 
+		catch (Exception exception) 
+		{
+			logger.error(exception.toString(),exception);
+			exception.printStackTrace();
+		}
+
+		ServiceProvider<Void> serviceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("dataIngestor")
+				.build();
+		try 
+		{
+			serviceProvider.start();
+		} 
+		catch (Exception exception) 
+		{
+			logger.error(exception.toString(),exception);
+			exception.printStackTrace();
+		}
+		
+		@SuppressWarnings("rawtypes")
+		ServiceInstance instance;		
+		
+		try 
+		{
+			List<ServiceInstance<Void>> instances =(List<ServiceInstance<Void>>) serviceProvider.getAllInstances();
+			if ( instances.size() == 0 )
+	        {
+	            return null;
+	        }
+			
+	        int thisIndex = DataIngestorService.getIndex();
+	        DataIngestorService.setIndex(thisIndex+1);
+	        System.out.println("thisIndex: " + thisIndex);
+	        System.out.println(instances.get(thisIndex % instances.size()));
+	        	       
+			String address = instances.get(thisIndex % instances.size()).getId();
+			UriSpec uriSpec = instances.get(thisIndex % instances.size()).getUriSpec();
+			String url = uriSpec.build();
+			System.out.println("URL: " + url);
+			System.out.println("Address: " + address);
+			return url;
+			
+		} 
+		catch (Exception exception) 
+		{
+			logger.error(exception.toString(),exception);
+			exception.printStackTrace();
+		}
+		
+	    return "Delegated!";	    
+	}
+	
+}
---
+++ api-ingestor/src/main/java/com/sciencegateway/dataingestor/resource/Testing.java
@@ -0,0 +1,48 @@
+package com.sciencegateway.dataingestor.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.json.JSONObject;
+
+//This is just a test class to test the init and delegate functionalities of Zookeeper
+
+@Path("/req")
+public class Testing 
+{
+
+	@GET
+	@Path("/test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response test()
+	{
+		ClientConfig clientConfigR = new ClientConfig();
+		Client clientR = ClientBuilder.newClient(clientConfigR);
+		clientR.property(ClientProperties.CONNECT_TIMEOUT, 5000);
+		WebTarget targetR = clientR.target("http://localhost:8080/dataingestor/webapi/ingestor/delegate");
+		String url = targetR.request().get(String.class);
+		System.out.println(url);
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("stationName", "KIND");
+		jsonObject.put("date", "2000-08-28");
+		jsonObject.put("time", "123456");
+		jsonObject.put("requestId", "1001");
+		jsonObject.put("userName", "Sneha");
+		ClientConfig clientConfig = new ClientConfig();
+		Client client = ClientBuilder.newClient(clientConfig);
+		clientR.property(ClientProperties.CONNECT_TIMEOUT, 5000);
+		WebTarget target = client.target("http://localhost:8080/dataingestor/webapi/service/url");
+		return target.request().post(Entity.entity(jsonObject.toString(), "application/json"),Response.class);
+	}
+	
+}
