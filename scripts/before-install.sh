e06f7d88b98ea39a5b5fa3bcf299e35a98b4c916
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-11-18 04:14:44 -0500

    Added zookeeper installation

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,6 +1,25 @@
 echo 'killing existing process on port 8000 if any'
 sudo kill -9 $(sudo lsof -i :8000 | grep LISTEN) >> /var/log/processkill.log 2>&1 &
 
+install_dir="/usr/local"
+dir="/usr/local/zookeeper-3.4.9"
+if [ ! -d "$dir" ] ; then
+	cd "$install_dir"
+	wget http://www-us.apache.org/dist/zookeeper/stable/zookeeper-3.4.9.tar.gz
+	tar xzf zookeeper-3.4.9.tar.gz 
+	rm zookeeper-3.4.9.tar.gz 
+	cd zookeeper-3.4.9/conf
+	sudo mv zoo_sample.cfg zoo.cfg
+	oldString="dataDir=/temp/zookeeper"
+	newString="dataDir=/var/lib/zookeeper"
+	sed -i "s/$oldString/$newString/g" zoo.cfg
+	cd ..
+fi
+
+cd "$dir/bin"
+./zkServer.sh stop
+./zkServer.sh start
+
 echo 'Removing existing docker instances' >> /var/log/sdetector-docker.log 2>&1
 docker ps -a | grep 'sdetect_img' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 docker ps -a | grep 'sdetect_img' | awk '{print $1}' | xargs --no-run-if-empty docker rm
