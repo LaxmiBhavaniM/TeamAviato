8947018330ede61c3e5a5b77cae3b37befbfd81e
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-11-17 21:35:25 -0500

    Added zookeeper service registry

--- stormclustering/stormclustering.py
+++ stormclustering/stormclustering.py
@@ -7,6 +7,79 @@ from flask import request
 import requests
 from logging import FileHandler, WARNING
 
+import json
+import calendar, datetime, time
+from datetime import datetime
+from kazoo.client import KazooClient
+import logging
+import uuid
+from kazoo.exceptions import (
+    AuthFailedError,
+    ConfigurationError,
+    ConnectionClosedError,
+    ConnectionLoss,
+    NoNodeError,
+    NodeExistsError,
+    SessionExpiredError,
+    WriterNotClosedException,
+)
+
+def register_to_zookeeper():
+    logging.basicConfig(filename='zkregistry.log', level=logging.DEBUG, format="%(asctime)s - %(name)s - %(message)s",
+                        datefmt="%H:%M:%S", filemode='w')
+
+    zk = KazooClient(hosts='localhost:2181')
+    # zk = KazooClient(hosts='localhost:2181')
+    zk.start()
+    # ********** register service with zookeeper *********
+    serviceName = "stormClustering"
+    ipaddress = "ec2-35-164-24-104.us-west-2.compute.amazonaws.com"
+    serviceURI = "/stormclustering/v1/service/kml"
+    port = 31000
+    path = "http://" + ipaddress + ":" + str(port) + ":" + serviceName
+
+    try:  # create base
+        zk.create('/weather-predictor')
+    except Exception as e1:
+        print("Error while creating Weather-predictor znode", e1)
+        logging.error("Error while creating Weather-predictor znode %s" % str(e1))
+    else:
+        logging.debug("/weather-predictor znode created")
+
+    try:  # create service znode
+        zk.create('/weather-predictor/stormClustering')
+    except Exception as e2:
+        print("Error while creating /weather-predictor/stormClustering znode", e2)
+        logging.error("Error while creating /weather-predictor/stormClustering znode %s" % str(e2))
+    else:
+        logging.debug("/weather-predictor/stormClustering znode created")
+
+    zk.ensure_path("/weather-predictor/stormClustering")
+    print(zk.client_id)
+
+    try:
+        uniqueid = str(uuid.uuid4())
+        zk.create('/weather-predictor/stormClustering/' + uniqueid,
+                  json.dumps({'name': serviceName, 'id': uniqueid, 'address': ipaddress, 'port': port,
+                              'sslPort': None, 'payload': None,
+                              'registrationTimeUTC': (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds(),
+                              'serviceType': 'DYNAMIC',
+                              "uriSpec": {
+                                  "parts": [{"value": path, "variable": False}]
+                              }}, ensure_ascii=True).encode(),
+                  ephemeral=True)
+
+    except Exception as e3:
+        print("Error while creating weather-predictor/stormClustering znode", e3)
+        logging.error("Error while creating /weather-predictor/stormClustering child znode %s" % str(e3))
+    else:
+        logging.debug("/weather-predictor/stormClustering child znode created %s" % uniqueid)
+        # ******************REGISTERED************
+
+#register
+register_to_zookeeper()
+
+
 app = Flask(__name__)
 if not app.debug:
     file_handler = FileHandler('errorlog.txt')
@@ -46,21 +119,7 @@ def generatecluster():
         print("Couldn't connect to registry service.")
     # ---------------------------------------------------------
 
-    '''
-    # *********************************************************
-    # connect to forecast trigger
-    config = ConfigParser()
-    config.read('config.ini')
-    host2 = config.get('configData', 'ipaddress2')
-    port2 = config.get('configData', 'port2')
-    url2 = "http://" + host2 + ":" + port2 + "/forecasttrigger/v1/service/trigger"
-    print(url2)
-    data2 = {'userName': userName, 'requestId': requestId, 'data': cluster}
-    # POST REQUEST
-    headers2 = {'Content-type': 'application/json'}
-    r2 = requests.post(url2, data=json.dumps(data2, ensure_ascii=False), headers=headers2)
-    # *********************************************************
-    '''
+
     data2 = {'userName': userName, 'requestId': requestId, 'data': cluster}
 
     return json.dumps(data2, ensure_ascii=False)
