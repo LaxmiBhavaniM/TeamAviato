feaa2c82e483e2aab40c2b93ad408d46dc3ab962
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-11-19 18:28:03 -0500

    run as sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 echo 'starting installation process' >> /var/log/sga-teamaviato-ForecastTrigger-install.log
 #cd '/home/ec2-user/forecasttrigger'
+sudo su
 
 rm -r /home/ec2-user/forecastTrigger
 mv /home/ec2-user/ForecastTrigger  /home/ec2-user/forecastTrigger
@@ -7,12 +8,12 @@ cd /home/ec2-user/forecastTrigger/
 chmod 777 forecasttrigger
 cd forecasttrigger
 
-setup bridge network
-docker network ls | grep 'mynet123'
-if [ $? ne 0 ]; then
-  docker network create --subnet=172.18.0.0/16 mynet123
-fi
+#setup bridge network
+#docker network ls | grep 'mynet123'
+#if [ $? ne 0 ]; then
+#  docker network create --subnet=172.18.0.0/16 mynet123
+#fi
 
 docker build -t ftrigger_img .
-#docker run -d -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
-docker run -d --net mynet123 --ip 172.18.0.33 -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
+docker run -d -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
+#docker run -d --net mynet123 --ip 172.18.0.33 -p 32000:32000 --name api-ftrigger ftrigger_img >> sga-teamaviato-ForecastTrigger-docker-server.log 2>&1 &
