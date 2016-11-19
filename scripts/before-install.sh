3c77561ef5a77cf0cb2a6400ece3401f482fb959
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-11-19 06:08:37 -0500

    Reverted back to using docker images from directory

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,34 +1,6 @@
-
 echo 'killing existing process on port 8000 if any'
 sudo kill -9 $(sudo lsof -i :8000 | grep LISTEN) >> /var/log/processkill.log 2>&1 &
 
-install_dir="/usr/local"
-dir="/usr/local/zookeeper-3.4.9"
-if [ ! -d "$dir" ] ; then
-	cd "$install_dir"
-	wget http://www-us.apache.org/dist/zookeeper/stable/zookeeper-3.4.9.tar.gz
-	tar xzf zookeeper-3.4.9.tar.gz 
-	rm zookeeper-3.4.9.tar.gz 
-	cd zookeeper-3.4.9/conf
-	sudo mv zoo_sample.cfg zoo.cfg
-	oldString="dataDir=/temp/zookeeper"
-	newString="dataDir=/var/lib/zookeeper"
-	sed -i "s/$oldString/$newString/g" zoo.cfg
-	cd ..
-fi
-
-cd "$dir/bin"
-./zkServer.sh stop
-./zkServer.sh start
-
-echo 'install docker-io' >> /var/log/sdetector-docker.log 2>&1
-sudo yum install -y docker-io >> /var/log/sdetector-docker.log 2>&1
-sudo service docker start >> /var/log/sdetector-docker.log 2>&1
-
-echo 'Removing existing docker instances' >> /var/log/sdetector-docker.log 2>&1
-docker ps -a | grep 'api-sdetect' | awk '{print $1}' | xargs --no-run-if-empty docker stop >> /var/log/sdetector-docker.log 2>&1
-docker ps -a | grep 'api-sdetect' | awk '{print $1}' | xargs --no-run-if-empty docker rm >> /var/log/sdetector-docker.log 2>&1
-
-#Remove existing images if any
-echo 'Removing existing docker images if any'>> /var/log/sdetector-docker.log 2>&1
-docker images | grep -w "laxmibhavanim/stormdetector" | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f >> /var/log/sdetector-docker.log 2>&1
+echo 'Removing existing docker instances' >> /var/log/scluster-docker.log 2>&1
+docker ps -a | grep 'sdetect_img' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+docker ps -a | grep 'sdetect_img' | awk '{print $1}' | xargs --no-run-if-empty docker rm
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,11 @@
-echo 'starting installation process' >> /var/log/sga-teamaviato-StormDetector-install.log
-#cd '/home/ec2-user/stormdetection'
+echo 'starting installation process' >> /var/log/sga-teamaviato-StormDetection-install.log
+#cd '/home/ec2-user/stormclustering'
 
-# copy artifacts to new dir
-mkdir '/home/ec2-user/stormdetector-microservice'
-cp -r /home/ec2-user/stormdetection/* /home/ec2-user/stormdetector-microservice
+rm -r /home/ec2-user/stormDetector
+mv /home/ec2-user/StormDetector  /home/ec2-user/stormDetector
+cd /home/ec2-user/stormDetector/
+chmod 777 stormdetection
+cd stormdetection
 
-# delete revision
-rm -rf '/home/ec2-user/stormdetection'
-
-chmod 777 stormdetector-microservice
-echo 'Installing the Storm Detector API...'
-cd '/home/ec2-user/stormdetector-microservice/stormdetection'
-
-
-cd '/home/ec2-user/docker'
-docker login -e="laxmibh.malkareddy@gmail.com" -u="laxmibhavanim" -p="laxmalka" >> /var/log/sdetector-docker-install.log 2>&1
-docker pull laxmibhavanim/stormdetector >> /var/log/sdetector-docker-install.log 2>&1
-docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f >> /var/log/sdetector-docker-install.log 2>&1
-docker run -d -p 8000:8000 --name api-sdetect $(docker images | grep -w "laxmibhavanim/stormdetector" | awk '{print $3}') >> /var/log/sdetector-docker-install.log 2>&1 &
-
-
-
+docker build -t sdetect_img .
+docker run -d -p 8000:8000 --name api-sdetect sdetect_img >> sga-teamaviato-StormDetection-docker-server.log 2>&1 &
