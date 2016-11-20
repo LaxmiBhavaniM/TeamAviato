c5efd46258c91aaa1cfa3082d7fd7223c1851360
Sneha Tilak <sneha.tilak26@gmail.com>
2016-11-19 22:31:27 -0500

    Updated zookeeper install steps #41

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -14,20 +14,21 @@ echo 'check if Zookeeper is installed'
 install_dir="/usr/local"
 dir="/usr/local/zookeeper-3.4.8"
 if [ ! -d "$dir" ] ; then
-	cd "$install_dir"
-	wget http://www-us.apache.org/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz
-	tar xzf zookeeper-3.4.8.tar.gz 
-	rm zookeeper-3.4.8.tar.gz 
-	cd "$dir/conf"
-	sudo touch zoo.cfg
-	echo "tickTime=2000" > zoo.cfg
-	echo "initLimit=10" >> zoo.cfg
-	echo "syncLimit=5" >> zoo.cfg
-	echo "dataDir=/var/lib/zookeeper" >> zoo.cfg
-	echo "clientPort=2181" >> zoo.cfg
-	echo "server.1=52.15.57.97:2888:3888" >> zoo.cfg
-	echo "server.2=35.164.24.104:2888:3888" >> zoo.cfg
-	echo "server.3=52.15.40.136:2888:3888" >> zoo.cfg
+        cd "$install_dir"
+        sudo wget http://www-us.apache.org/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz
+        sudo tar xzf zookeeper-3.4.8.tar.gz
+        sudo rm zookeeper-3.4.8.tar.gz
+        cd "$dir/conf"
+        sudo touch zoo.cfg
+        sudo chmod 777 zoo.cfg
+        echo "tickTime=2000" > zoo.cfg
+        echo "initLimit=10" >> zoo.cfg
+        echo "syncLimit=5" >> zoo.cfg
+        echo "dataDir=/var/lib/zookeeper" >> zoo.cfg
+        echo "clientPort=2181" >> zoo.cfg
+        echo "server.1=52.15.57.97:2888:3888" >> zoo.cfg
+        echo "server.2=35.164.24.104:2888:3888" >> zoo.cfg
+        echo "server.3=52.15.40.136:2888:3888" >> zoo.cfg
 fi
 
 cd "$dir/bin"
