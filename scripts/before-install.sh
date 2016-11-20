7a668c758ed1232b02ace6b2157b6b09b88112b6
LaxmiBhavaniM <laxmibh.malkareddy@gmail.com>
2016-11-19 19:37:15 -0500

    run as sudo

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,6 +1,8 @@
 echo 'killing existing process on port 8050 if any'
 sudo kill -9 $(sudo lsof -i :8050 | grep LISTEN) >> /var/log/processkill.log 2>&1 &
 
+sudo su
+
 echo 'Removing existing docker instances' >> /var/log/runforecast-docker.log 2>&1
 docker ps -a | grep 'runforecast_img' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 docker ps -a | grep 'runforecast_img' | awk '{print $1}' | xargs --no-run-if-empty docker rm
