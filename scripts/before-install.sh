feb19e69769f2f48b8f81e440085c37cfe19a8f4
Laxmi Bhavani Malkareddy <laxmibh.malkareddy@gmail.com>
2016-11-20 16:21:34 -0500

    Added zookeeper install scripts

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,6 +3,32 @@ sudo kill -9 $(sudo lsof -i :32000 | grep LISTEN) >> /var/log/processkill.log 2>
 
 sudo su
 
+echo 'check if Zookeeper is installed'
+install_dir="/usr/local"
+dir="/usr/local/zookeeper-3.4.8"
+if [ ! -d "$dir" ] ; then
+        cd "$install_dir"
+        sudo wget http://www-us.apache.org/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz
+        sudo tar xzf zookeeper-3.4.8.tar.gz
+        sudo rm zookeeper-3.4.8.tar.gz
+        cd "$dir/conf"
+        sudo touch zoo.cfg
+        sudo chmod 777 zoo.cfg
+        echo "tickTime=2000" > zoo.cfg
+        echo "initLimit=10" >> zoo.cfg
+        echo "syncLimit=5" >> zoo.cfg
+        echo "dataDir=/var/lib/zookeeper" >> zoo.cfg
+        echo "clientPort=2181" >> zoo.cfg
+        echo "server.1=52.15.57.97:2888:3888" >> zoo.cfg
+        echo "server.2=35.164.24.104:2888:3888" >> zoo.cfg
+        echo "server.3=52.15.40.136:2888:3888" >> zoo.cfg
+fi
+
+cd "$dir/bin"
+./zkServer.sh stop
+./zkServer.sh start
+
+
 echo 'Removing existing docker instances' >> /var/log/ftrigger-docker.log 2>&1
 docker ps -a | grep 'ftrigger_img' | awk '{print $1}' | xargs --no-run-if-empty docker stop
 docker ps -a | grep 'ftrigger_img' | awk '{print $1}' | xargs --no-run-if-empty docker rm
