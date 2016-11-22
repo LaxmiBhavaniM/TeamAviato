b491d8414e6fdf6d9bbabb9af279404971645d3a
Sneha Tilak <sneha.tilak26@gmail.com>
2016-11-22 17:23:35 -0500

    Rectified Tomcat install steps

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,20 +1,20 @@
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64
 
 echo 'check if tomcat is installed'
-cd /usr/local/tomcat7/apache-tomcat-7.0.72 
+cd /usr/local/tomcat7/apache-tomcat-7.0.73 
 sudo ./bin/version.sh
 if [ "$?" -ne 0 ]; then
    echo 'Installing Tomcat...'
-   	sudo wget http://www.us.apache.org/dist/tomcat/tomcat-7/v7.0.72/bin/apache-tomcat-7.0.72.tar.gz
-	tar xzf apache-tomcat-7.0.72.tar.gz
+   	sudo wget http://www.us.apache.org/dist/tomcat/tomcat-7/v7.0.73/bin/apache-tomcat-7.0.73.tar.gz
+	tar xzf apache-tomcat-7.0.73.tar.gz
 	sudo mkdir /usr/local/tomcat7
-	sudo mv apache-tomcat-7.0.72 /usr/local/tomcat7
-	cd /usr/local/tomcat7/apache-tomcat-7.0.72
+	sudo mv apache-tomcat-7.0.73 /usr/local/tomcat7
+	cd /usr/local/tomcat7/apache-tomcat-7.0.73
 	sudo ./bin/startup.sh
 	sudo ./bin/version.sh
 else
    echo 'Killing existing tomcat process if any'
-	cd /usr/local/tomcat7/apache-tomcat-7.0.72
+	cd /usr/local/tomcat7/apache-tomcat-7.0.73
 	sudo sh ./bin/shutdown.sh
 	sleep 20
 fi
